f91e6f1ca721f1600abb4a499a0a502866868c76
Prem shankar Garabadu <premshankargarabadu@gmail.com>
2019-08-01 17:55:16 +0530

    created models for bank

--- bank/models.py
+++ bank/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Banks(models.Model):
+    class Meta:
+        db_table = 'banks'
+    name = models.CharField(max_length=49)
+
+    def __str__(self):
+        return self.name
+
+
+class Branches(models.Model):
+
+    class Meta:
+        db_table = 'branches'
+    ifsc = models.CharField(max_length=11, primary_key=True, null=False)
+    bank_id = models.ForeignKey(Banks, on_delete=models.CASCADE)
+    branch = models.CharField(max_length=74)
+    address = models.CharField(max_length=195)
+    city = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    state = models.CharField(max_length=26)
+
+
+class BankBranches(models.Model):
+
+    class Meta:
+        managed = False
+        db_table = 'bank_branches'
+
+    ifsc = models.CharField(max_length=11, primary_key=True, null=False)
+    bank_id = models.ForeignKey(Banks, on_delete=models.CASCADE)
+    branch = models.CharField(max_length=74)
+    address = models.CharField(max_length=195)
+    city = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    state = models.CharField(max_length=26)
+    bank_name = models.CharField(max_length=49)
+
