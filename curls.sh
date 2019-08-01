8a4c1641b9a8d60b1788f24f420d50e76e1e856b
Prem shankar Garabadu <premshankargarabadu@gmail.com>
2019-08-02 01:08:04 +0530

    updated curls script

--- curls.sh
+++ curls.sh
@@ -3,13 +3,13 @@
 
 # GET API to fetch a bank details, given branch IFSC code
 echo -e '\nGET API to fetch a bank details, given branch IFSC code\n\n'
- curl -X GET http://127.0.0.1:8000/bank/ABHY0065001 -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTY1MTE0Njc2LCJqdGkiOiI3ZGJhMjg1NTViMTQ0YjQ3OTUyMGQyYjJkZjJkNGY4ZiIsInVzZXJfaWQiOjF9.EyWP7SZpjgF9NUwDbwrfptFW-ybdpG08WcMnDGSFOZk'
+ curl -X GET 'https://calm-wave-12873.herokuapp.com/bank/ABHY0065001?limit=1&offset=0' -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTY1MTIwMDkwLCJqdGkiOiI3ZjVmOTAxNDI3NGI0YmI5OWI5N2FiNDYxY2NjMjUzNiIsInVzZXJfaWQiOjF9.PqmklrjadSjNHUsscqeor6dgdOl3ZJvlNnD7ZN9ZjtE'
 
 
 # GET API to fetch all details of branches, given bank name and a city
 echo -e '\n\n\n'
 
 echo -e '\nGET API to fetch all details of branches, given bank name and a city\n\n'
-curl -X GET 'http://127.0.0.1:8000/bank/ABHYUDAYA%20COOPERATIVE%20BANK%20LIMITED/MUMBAI?limit=10&offset=20' -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTY1MTE0Njc2LCJqdGkiOiI3ZGJhMjg1NTViMTQ0YjQ3OTUyMGQyYjJkZjJkNGY4ZiIsInVzZXJfaWQiOjF9.EyWP7SZpjgF9NUwDbwrfptFW-ybdpG08WcMnDGSFOZk'
+curl -X GET 'https://calm-wave-12873.herokuapp.com/bank/ABHYUDAYA%20COOPERATIVE%20BANK%20LIMITED/MUMBAI?limit=10&offset=20' -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTY1MTIwMDkwLCJqdGkiOiI3ZjVmOTAxNDI3NGI0YmI5OWI5N2FiNDYxY2NjMjUzNiIsInVzZXJfaWQiOjF9.PqmklrjadSjNHUsscqeor6dgdOl3ZJvlNnD7ZN9ZjtE'
 echo -e '\n'
 
