b19a5bc5ed506e6049a590a28fbaa3f561cbbc24
Prem shankar Garabadu <premshankargarabadu@gmail.com>
2019-08-02 01:44:22 +0530

    added a landing page

--- bank/urls.py
+++ bank/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt import views as jwt_views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -8,4 +9,7 @@ urlpatterns = [
     path('bank/<bank_name>/<city>', views.BranchesdetailsViewSet.as_view({'get': 'list'})),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('', TemplateView.as_view(template_name="index.html"))
 ]
+
+
