2f3a1fbc77aab97d0c9b6a71141ad09a7002872f
Prem shankar Garabadu <premshankargarabadu@gmail.com>
2019-08-02 21:36:36 +0530

    done typeconversion in settings

--- Indian_bank/settings.py
+++ Indian_bank/settings.py
@@ -141,10 +141,10 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=config('ACCESS_TOKEN_LIFETIME')),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=config('REFRESH_TOKEN_LIFETIME')),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=int(config('ACCESS_TOKEN_LIFETIME'))),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=int(config('REFRESH_TOKEN_LIFETIME'))),
 
 }
 
-prod_db  =  dj_database_url.config(conn_max_age=config(conn_max_age))
+prod_db  =  dj_database_url.config(conn_max_age=int(config('conn_max_age')))
 DATABASES['default'].update(prod_db)
